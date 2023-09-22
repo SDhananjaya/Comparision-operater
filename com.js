01268b6f78770f82620215ff7b96287bada8bb4f
dhananjaya5023 <dhananjaya5023@gmail.com>
2023-09-22 16:38:59 -0400

    comparision

---
+++ com.js
@@ -0,0 +1,16 @@
+document.write("<p>A=10 ,B=5</p>");
+
+var a=10;
+var b=5;
+document.write("<br/>");
+document.write("result of A==B "+(a==b));
+document.write("<br/>");
+document.write("result of A!=B :"+(a!=b));
+document.write("<br/>");
+document.write("result of A>B :"+(a>b));
+document.write("<br/>")
+document.write("result of A< B :"+(a<b));
+document.write("<br/>");
+document.write("result of A>=B :"+(a>=b));
+document.write("<br/>");
+document.write("result of A<=B :"+(a<=b));
